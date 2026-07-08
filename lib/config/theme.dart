1bfb291b98b5d5acd77fbb67ec4889221a6ac76e
Erik Armenta <da6740284@gmail.com>
2026-07-08 15:24:20 -0600

    Update theme.dart

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -27,7 +27,7 @@ class EaTheme {
       ),
       scaffoldBackgroundColor: const Color(0xFF0F1117),
       textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: EaColors.card,
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
