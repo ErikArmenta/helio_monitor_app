f1bdabf8c8896fe0b001c9439db7d9874911b14e
Erik Armenta <da6740284@gmail.com>
2026-07-08 14:46:28 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 echo "🔄 Descargando Flutter desde GitHub..."
 git clone --depth 1 https://github.com/flutter/flutter.git -b stable
 
+echo "⚙️  Configurando el proyecto para la web..."
+./flutter/bin/flutter create . --platforms web
+
 echo "🔧 Construyendo la Web..."
 ./flutter/bin/flutter build web --release \
   --dart-define=SUPABASE_URL=$SUPABASE_URL \
